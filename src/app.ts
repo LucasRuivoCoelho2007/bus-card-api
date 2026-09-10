beab130170373a2375715209d568c9c8154e8dac
Lucas Ruivo Coelho <lucasruivoescola@gmail.com>
2026-09-10 09:47:06 -0300

    feat: implement CRUD operations for card management

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import { Application } from "jsr:@oak/oak";
 
-import userRouter from "./routes/user.routes.ts";
-import authRouter from "./routes/auth.routes.ts";
 import healthRouter from "./routes/health.routes.ts";
+import userRouter from "./routes/user.routes.ts";
+import cardRouter from "./routes/card.routes.ts";
+import authRouter from "./routes/auth.routes.ts";
 
 
 const app = new Application();
@@ -15,6 +16,9 @@ app.use(userRouter.allowedMethods());
 
 app.use(authRouter.routes());
 app.use(authRouter.allowedMethods());
+
+app.use(cardRouter.routes());
+app.use(cardRouter.allowedMethods());
 
 console.log("Server running on http://localhost:8000");
 
---
+++ src/repositories/card.repository.ts
@@ -0,0 +1,41 @@
+import { cards } from "../config/database.ts";
+import type { ICard } from "../models/card.ts";
+import { ObjectId } from "npm:mongodb";
+
+export class CardRepository {
+  async create(card: ICard): Promise<ICard> {
+    await cards.insertOne(card);
+
+    return card;
+  }
+
+  async getCards(userId: string) {
+    const userCards = await cards.find({
+      user_id: new ObjectId(userId),
+    }).toArray();
+
+    return userCards;
+  }
+
+  async getCardById(cardId: string, userId: string) {
+    const card = await cards.findOne({
+      _id: new ObjectId(cardId),
+      user_id: new ObjectId(userId),
+    });
+
+    return card;
+  }
+
+  async updateCard(cardId: string, userId: string, updatedCard: Partial<ICard>) {
+    await cards.updateOne({
+      _id: new ObjectId(cardId),
+      user_id: new ObjectId(userId),
+    }, { $set: updatedCard });
+  }
+  async deleteCard(cardId: string, userId: string) {
+    await cards.deleteOne({
+      _id: new ObjectId(cardId),
+      user_id: new ObjectId(userId),
+    });
+  }
+}
